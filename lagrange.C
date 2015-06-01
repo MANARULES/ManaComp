10e467d6e06da97701a2f645eb198197f694633d
filipematos95 <filipematos@Air-de-Filipe-2.home>
2015-06-02 00:13:11 +0100

    Finally is working now

--- lagrange.C
+++ lagrange.C
@@ -95,7 +95,7 @@ void action::graph_gnuplot(int a, int b, string c){
     if( b != 0){
       Gnuplot g2("lines");
       g2.set_grid();
-      g2.set_style("lines").plot_xy(t,vx,"vx(t)");
+      g2.set_style("points").plot_xy(t,vx,"vx(t)");
     }
     if ( c != NULL){
       Gnuplot g3("lines");
@@ -116,8 +116,8 @@ void action::lagrange1()
 {
   //Variable Definition
   vector<double> xp, xdot, S, points, time ;
-  int n = 50;
-  double deltat=0,deltax=0, epsilon=0.001, delta=0.00001, aux, x1, x2, xdot1, xdot2, xm1, xm2;
+  int n = 20;
+  double deltat=0,deltax=0, epsilon=0.0001, delta=0.00001, aux, x1, x2, xdot1, xdot2, xm1, xm2;
   TF2 *lagrangian = lag2;
   double action = 0;
   
@@ -143,104 +143,95 @@ void action::lagrange1()
   }
   
   cout << "action: " << action << endl; //total action when using straight line to connect the points
-  action =0;
-  //Minimizing action for each small interval
-  for(int i=0; i<n; i++)
-  {
-    double k = points[i]; //keeps the value of the point to enter the iteration
-    // cout << 1 << endl;
-    for(int j=1;j<1000000;j++) //increases values of position change
-    {
-      //Test interval for positive epsilon
-      /*Como variar a posição de um ponto varia a acção no intervalo anterior e posterior a este ponto
-       é necessário considerar uma minimização conjugada da acção nos dois intervalos daí a utilização
-       de variáveis 1 e 2. Para além disso, em cada iteração testa-se aumentar a posição e diminui-la. O valor extremo de j no for é relativo...*/
-      
-      x1 = k+j*epsilon;
-      xdot1 = (x1-points[i])/deltat;
-      xm1 = (points[i]+x1)/2;
-      
-      x2 = k+j*epsilon;
-      xdot2 = (points[i+2]-x2)/deltat;
-      xm2 = (points[i+2]+x2)/2;
-      
-      aux = deltat*(lagrangian->Eval(xm1,xdot1)+lagrangian->Eval(xm2,xdot2));
-      
-      if(S[i]+S[i+1]>aux)
-      {
-        //	cout << k << endl;
-        //	cout << points[i+1] << endl;
-        points[i+1] = x1;
-        xp[i] = xm1;
-        xdot[i] = xdot1;
-        
-        xp[i+1]=xm2;
-        xdot[i+1] = xdot2;
-        
-        double a = S[i];
-        double b = S[i+1];
-        
-        S[i]=deltat*(lagrangian->Eval(xp[i],xdot[i],0));
-        S[i+1] = deltat*(lagrangian->Eval(xp[i+1],xdot[i+1],0));
-        
-        if(abs((a+b)-(S[i]+S[i+1]))<delta)
-        {
-          cout << "breakou" << endl;
-          break;
-        }
-      }
-      
-      //Test interval for negative epsilon
-      
-      x1 = k-j*epsilon;
-      xdot1 = (x1-points[i])/deltat;
-      xm1 = (points[i]+x1)/2;
-      
-      x2 = k-j*epsilon;
-      xdot2 = (points[i+2]-x2)/deltat;
-      xm2 = (points[i+2]+x2)/2;
-      
-      aux = deltat*(lagrangian->Eval(xm1,xdot1,0)+lagrangian->Eval(xm2,xdot2,0));
-      
-      if(S[i]+S[i+1]>aux)
-      {
-        //	cout << k << endl;
-        //	cout << points[i+1] << endl;
-        points[i+1] = x1;
-        xp[i] = xm1;
-        xdot[i] = xdot1;
-        
-        xp[i+1]=xm2;
-        xdot[i+1] = xdot2;
-        
-        double a = S[i];
-        double b = S[i+1];
-        
-        S[i]=deltat*(lagrangian->Eval(xp[i],xdot[i],0));
-        S[i+1] = deltat*(lagrangian->Eval(xp[i+1],xdot[i+1],0));
-        
-        if(abs((a+b)-(S[i]+S[i+1]))<delta)
-        {
-          cout << "breakou5" << endl;
-          break;
-        }
-      }
-      
-    }
-  }
-  action = 0;
-  for(int i=0; i<n;i++)
-    action = action +S[i];
   
-  cout << "action: "<< action << endl;
-  x = points;
-  cout << "x.size()" << x.size() << endl;
-  cout << "vx.size()" << xdot.size() << endl;
+  //Minimizing action for each small interval
+  for(int m=0; m<1000; m++)
+  {
+    double actionaux = action;
+    action = 0;
+    for(int i=0; i<n-1; i++)
+    {
+      double k = points[i+1]; //keeps the value of the point to enter the iteration
+      // cout << 1 << endl;
+      for(int j=1;j<100;j++) //increases values of position change
+      {
+        //Test interval for positive epsilon
+        /*Como variar a posição de um ponto varia a acção no intervalo anterior e posterior a este ponto
+         é necessário considerar uma minimização conjugada da acção nos dois intervalos daí a utilização
+         de variáveis 1 e 2. Para além disso, em cada iteração testa-se aumentar a posição e diminui-la. O valor extremo de j no for é relativo...*/
+        
+        x1 = k+j*epsilon;
+        xdot1 = (x1-points[i])/deltat;
+        xm1 = (points[i]+x1)/2;
+        
+        x2 = k+j*epsilon;
+        xdot2 = (points[i+2]-x2)/deltat;
+        xm2 = (points[i+2]+x2)/2;
+        
+        aux = deltat*(lagrangian->Eval(xm1,xdot1)+lagrangian->Eval(xm2,xdot2));
+        
+        if(S[i]+S[i+1]>aux)
+        {
+          //	cout << k << endl;
+          //	cout << points[i+1] << endl;
+          points[i+1] = x1;
+          xp[i] = xm1;
+          xdot[i] = xdot1;
+          
+          xp[i+1]=xm2;
+          xdot[i+1] = xdot2;
+          
+          double a = S[i];
+          double b = S[i+1];
+          
+          S[i]=deltat*(lagrangian->Eval(xp[i],xdot[i],0));
+          S[i+1] = deltat*(lagrangian->Eval(xp[i+1],xdot[i+1],0));
+        }
+        
+        //Test interval for negative epsilon
+        
+        x1 = k-j*epsilon;
+        xdot1 = (x1-points[i])/deltat;
+        xm1 = (points[i]+x1)/2;
+        
+        x2 = k-j*epsilon;
+        xdot2 = (points[i+2]-x2)/deltat;
+        xm2 = (points[i+2]+x2)/2;
+        
+        aux = deltat*(lagrangian->Eval(xm1,xdot1,0)+lagrangian->Eval(xm2,xdot2,0));
+        
+        if(S[i]+S[i+1]>aux)
+        {
+          //	cout << k << endl;
+          //	cout << points[i+1] << endl;
+          points[i+1] = x1;
+          xp[i] = xm1;
+          xdot[i] = xdot1;
+          
+          xp[i+1]=xm2;
+          xdot[i+1] = xdot2;
+          
+          double a = S[i];
+          double b = S[i+1];
+          
+          S[i]=deltat*(lagrangian->Eval(xp[i],xdot[i],0));
+          S[i+1] = deltat*(lagrangian->Eval(xp[i+1],xdot[i+1],0));
+        }
+        
+      }
+      action = action +S[i];
+    }
+    // cout << points.size()<<endl;
+    //  cout << time.size()<<endl;
+    //  cout << "x" <<x.size()<<endl;
+    if(abs(actionaux-action)<delta)
+      break;
+  }
+  xdot.push_back(xdot.back());
   vx = xdot;
-  vx.push_back(30);
+  x = points;
   t = time;
 
-  
 }
 
 //Here ends the lagrange1 funcion
@@ -282,5 +273,5 @@ int main(int argc, char *argv[]){
   }
   action a1 = action(x_i,x_f,t_i,t_f,lag);
   a1.lagrange1();
-  a1.graph_gnuplot(1,1,"-0.25*x*x"); // The first argument is if you wanna plot x(t) i so just put 1 if not put 0. The second is the same but this time for xponto(t). O terceiro é um plot de uma função que ponhas lá, to compare  with the x(t) or xponto(t) real...
+  a1.graph_gnuplot(1,1,"-0.5*x*x+20"); // The first argument is if you wanna plot x(t) i so just put 1 if not put 0. The second is the same but this time for xponto(t). O terceiro é um plot de uma função que ponhas lá, to compare  with the x(t) or xponto(t) real...
 }
